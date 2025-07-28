cacd8b2571013d53dab3d4efef7a0d4207836795
Manish kumar Das <manishpandya669@gmail.com>
2025-07-28 14:00:59 +0530

    upadated index.html and main.js , added API folder

--- zintargo/src/main.js
+++ zintargo/src/main.js
@@ -1,22 +1,4 @@
-import './style.css'
+import "src/style.css";
 
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Manish!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+import products from "./api/products.json";
+console.log(products);
